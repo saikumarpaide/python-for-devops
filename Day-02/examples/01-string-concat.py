5284b99c3f72cd3a3e9cdaefa0e70ca69c4f5f34
Saikumar Paide <saikumarpaide796@gmail.com>
2023-11-25 19:29:55 +0530

    updated

--- Day-02/examples/01-string-concat.py
+++ Day-02/examples/01-string-concat.py
@@ -1,4 +1,11 @@
-str1 = "Hello"
-str2 = "World"
-result = str1 + " " + str2
+str1 = "Shourya Teja"
+str2 = "Age : 2 +"
+result = str1 +"\n"+ str2
 print(result)
+print("Length of String 1 = ",len(str1))
+print("length of String 2 = ",len(str2))
+print("length of result = ",len(result))
+
+text = "      Hello"
+print("Length of text: ",len(text))
+print("Length of Stripped Text: ", len(text.strip()))
--- Day-02/examples/01-string-lowercase.py
+++ Day-02/examples/01-string-lowercase.py
@@ -3,3 +3,8 @@ uppercase = text.upper()
 lowercase = text.lower()
 print("Uppercase:", uppercase)
 print("Lowercase:", lowercase)
+
+small_to_large = "saikumar paide"
+temp = small_to_large.upper()
+print(small_to_large.upper())
+print(temp.lower())
