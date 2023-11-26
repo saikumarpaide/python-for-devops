94e111c47a39b4c25ace85f25e1162eec7f99ea1
Saikumar Paide <saikumarpaide796@gmail.com>
2023-11-26 13:28:57 +0530

    calc program

---
+++ Day-04/Calc.py
@@ -0,0 +1,19 @@
+num1 = int(input("Imput First Number :-    "))
+num2 = int(input("Input Second Number :-   "))
+
+def addition():
+    print(num1+num2)
+
+def substraction():
+    print(num1-num2)
+
+def multiplication():
+    print(num1 * num2)
+
+def module():
+    print(num1 % num2)
+
+addition()
+substraction()
+multiplication()
+module()
